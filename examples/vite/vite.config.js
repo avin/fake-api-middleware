6497d3de72f297e41606b2a524e5dd0a0154c975
a.vinogradov <a.vinogradov@bpcbt.com>
2023-01-08 16:53:35 +0300

    upd

--- examples/vite/vite.config.js
+++ examples/vite/vite.config.js
@@ -2,20 +2,20 @@ import { defineConfig } from 'vite';
 import { vitePlugin as fakeResponseVitePlugin } from '../../dist/index';
 
 export default defineConfig({
-  plugins: [
-    fakeResponseVitePlugin({
-      responsesFile: './apiMock/index.ts',
-      watchFiles: ['./apiMock/*'],
-    }),
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://jsonplaceholder.typicode.com',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
+  // plugins: [
+  //   fakeResponseVitePlugin({
+  //     responsesFile: './apiMock/index.ts',
+  //     watchFiles: ['./apiMock/*'],
+  //   }),
+  // ],
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://jsonplaceholder.typicode.com',
+  //       changeOrigin: true,
+  //       secure: false,
+  //       rewrite: (path) => path.replace(/^\/api/, ''),
+  //     },
+  //   },
+  // },
 });
