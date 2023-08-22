ee31d104bcc7e367983e9bea5eb4b50c5c0e886c
Avin Lambrero <avin.github@gmail.com>
2023-08-22 14:22:23 +0300

    Remove lodash (#11)

--- src/helpers.ts
+++ src/helpers.ts
@@ -3,3 +3,31 @@ export const delay = (time: number): Promise<void> => {
     setTimeout(r, time);
   });
 };
+
+export const isString = (value: any): value is string => {
+  return typeof value === 'string' || value instanceof String;
+};
+
+export const debounce = <T extends (...args: any[]) => any>(
+  func: T,
+  wait: number,
+  immediate = false,
+): ((...args: Parameters<T>) => void) => {
+  let timeout: ReturnType<typeof setTimeout> | null = null;
+  return (...args: Parameters<T>) => {
+    const later = () => {
+      timeout = null;
+      if (!immediate) {
+        func.apply(this, args);
+      }
+    };
+    const callNow = immediate && !timeout;
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(later, wait);
+    if (callNow) {
+      func.apply(this, args);
+    }
+  };
+};
